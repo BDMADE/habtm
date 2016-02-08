c0b7842ec0c18afed26cccda3d70a8e16cdb3979
hmtanbir <tanbir2025@gmail.com>
2016-02-08 21:00:33 +0600

    Customer and order Model are created and these are joined also by habtm

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :orders
+  resources :customers
   resources :candies
   resources :kids
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160207132217) do
+ActiveRecord::Schema.define(version: 20160208125732) do
 
   create_table "candies", force: :cascade do |t|
     t.string   "name"
@@ -24,7 +24,24 @@ ActiveRecord::Schema.define(version: 20160207132217) do
     t.integer "kid_id"
   end
 
+  create_table "customers", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers_orders", id: false, force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "order_id",    null: false
+  end
+
   create_table "kids", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
